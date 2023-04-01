700d7a0f08a14e6ee31973fd9b84b9386756f10d
Ivan Andrade <ivan.batapum@gmail.com>
2023-04-01 01:21:33 -0300

    Add current sensor

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,30 @@
 #include <Agentuino.h>                                                              
 #include <DHT.h>                                                                    
 
-#define DHTPIN 2                                                                  
+
+#define DHTPIN 5                                                                  
 #define DHTTYPE DHT22                                                              
-DHT dht(DHTPIN, DHTTYPE);                                                           // Setup object sensor
+DHT dht(DHTPIN, DHTTYPE);                                                           
+
 
 // Network information
 static byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};                         
 static byte ip[] = {192, 168, 0, 95};                                         
-static byte ip_dns[] = {192, 168, 1, 1};                                          // Dont change
+static byte ip_dns[] = {192, 168, 1, 1};                                          
 static byte ip_gateway[] = { 192, 168, 0, 1};
 static byte subnet[] = {255, 255, 255, 0 };
 
-
-//Snesor variables
+// Sensor variables
 float humidity = 0;
 float temperature = 0;
+int current = 0; 
+
+const int voltage_delta = 2475; 
+const int relation_AmperVolt = 12;
+
 char result[8];
 
-
-// Device information OID 
+// Device info OID 
 const char sysDescr[] PROGMEM      = "1.3.6.1.2.1.1.1.0";                          
 const char sysContact[] PROGMEM    = "1.3.6.1.2.1.1.4.0";                           
 const char sysName[] PROGMEM       = "1.3.6.1.2.1.1.5.0";                           
@@ -33,26 +38,42 @@ const char sysLocation[] PROGMEM   = "1.3.6.1.2.1.1.6.0";
 const char sysServices[] PROGMEM   = "1.3.6.1.2.1.1.7.0";                           
  
 // Sensor OID
-const char snmp_temperature[]     PROGMEM     = "1.3.6.1.3.2016.5.1.0";             // Temperature in celsius
-const char snmp_humidity[]        PROGMEM     = "1.3.6.1.3.2016.5.1.1";             // percentage humidity
+const char snmp_temperature[]     PROGMEM     = "1.3.6.1.3.2016.5.1.0";             
+const char snmp_humidity[]        PROGMEM     = "1.3.6.1.3.2016.5.1.1";             
 const char snmp_current[]        PROGMEM     = "1.3.6.1.3.2016.5.1.2"; 
 
 // RFC1213 local values
-static char locDescr[35]            = "SNMPv1 - System Sensing v1.1";               // read-only (static)
-static char locContact[25]          = "Silica Networks SA";                         // should be stored/read from EEPROM - read/write (dont for simplicity)
-static char locName[20]             = "NOC";                                        // should be stored/read from EEPROM - read/write (dont for simplicity)
-static char locLocation[20]         = "SMA638 - CABA";                              // should be stored/read from EEPROM - read/write (dont for simplicity)
-static int32_t locServices          = 2;                                            // read-only (static)
+static char locDescr[35]            = "System Sensing v1.0";              
+static char locContact[25]          = "Silica Networks SA";                         
+static char locName[20]             = "A NOC product";                                        
+static char locLocation[20]         = "SMA 638 - CABA";                             
+static int32_t locServices          = 2;                                            
  
 uint32_t prevMillis = millis();
 char oid[SNMP_MAX_OID_LEN];
 SNMP_API_STAT_CODES api_status;
 SNMP_ERR_CODES status;
 
-// Calculate average of sensor
 
+// Return current value
+int deltaCurrent() {
+  float sum = 0;
+  float current_temp = 0;
+  float voltage = 0;
+
+  for (int i = 0; i < 3; i++) {           
+    voltage = (analogRead(0) * 5000.0) / 1023; 
+    current = (voltage - voltage_delta) / relation_AmperVolt;  
+    sum = current + sum;      
+    delay(200);        
+  }
+  current_temp = sum / 3;
+  return current_temp;
+}
  
-void pduReceived(){
+
+
+void pduReceived() {
   SNMP_PDU pdu;
   api_status = Agentuino.requestPdu(&pdu);
   
@@ -86,7 +107,6 @@ void pduReceived(){
         pdu.type = SNMP_PDU_RESPONSE;
         pdu.error = status;
       }
-      //
     } else if ( strcmp_P(oid, sysContact ) == 0 ) {
       // handle sysContact (set/get) requests
       if ( pdu.type == SNMP_PDU_SET ) {
@@ -100,7 +120,7 @@ void pduReceived(){
         pdu.type = SNMP_PDU_RESPONSE;
         pdu.error = status;
       }
-      //
+
     } else if ( strcmp_P(oid, sysLocation ) == 0 ) {
       // handle sysLocation (set/get) requests
       if ( pdu.type == SNMP_PDU_SET ) {
@@ -127,58 +147,59 @@ void pduReceived(){
         pdu.type = SNMP_PDU_RESPONSE;
         pdu.error = status;
       }
-      //
-    }
-    else if ( strcmp_P(oid, snmp_temperature ) == 0 ) 
-    {
+    } else if ( strcmp_P(oid, snmp_temperature ) == 0 ) {
       // handle sysName (set/get) requests
-      if ( pdu.type == SNMP_PDU_SET ) 
-      {      
+      if ( pdu.type == SNMP_PDU_SET ) {      
         // response packet from set-request - object is read-only
         pdu.type = SNMP_PDU_RESPONSE;
         pdu.error = SNMP_ERR_READ_ONLY;
       } 
-      else 
-      {
+      else {
         status = pdu.VALUE.encode(SNMP_SYNTAX_OCTETS, dtostrf(temperature,6,2,result));
         pdu.type = SNMP_PDU_RESPONSE;
         pdu.error = status;
       }
-    } 
-        else if ( strcmp_P(oid, snmp_humidity ) == 0 ) 
-    {
+    } else if ( strcmp_P(oid, snmp_humidity ) == 0 ) {
       // handle sysName (set/get) requests
-      if ( pdu.type == SNMP_PDU_SET ) 
-      {      
+      if ( pdu.type == SNMP_PDU_SET ) {      
         // response packet from set-request - object is read-only
         pdu.type = SNMP_PDU_RESPONSE;
         pdu.error = SNMP_ERR_READ_ONLY;
       } 
-      else 
-      {
+      else {
         status = pdu.VALUE.encode(SNMP_SYNTAX_OCTETS, dtostrf(humidity,6,2,result));
+        pdu.type = SNMP_PDU_RESPONSE;
+        pdu.error = status;
+      }
+    } else if ( strcmp_P(oid, snmp_current ) == 0 ) {
+      // handle sysName (set/get) requests
+      if ( pdu.type == SNMP_PDU_SET ) {      
+        // response packet from set-request - object is read-only
+        pdu.type = SNMP_PDU_RESPONSE;
+        pdu.error = SNMP_ERR_READ_ONLY;
+      } 
+      else {
+        status = pdu.VALUE.encode(SNMP_SYNTAX_OCTETS, dtostrf(current,6,2,result));
         pdu.type = SNMP_PDU_RESPONSE;
         pdu.error = status;
       }
     }   
     else {
-      // oid does not exist
+      // oid don't exist
       // response packet - object not found
       pdu.type = SNMP_PDU_RESPONSE;
       pdu.error = SNMP_ERR_NO_SUCH_NAME;
     }
     Agentuino.responsePdu(&pdu);
   }
- 
   Agentuino.freePdu(&pdu);
- 
 }
  
+
 void setup() {
   dht.begin();
-  
-  Ethernet.begin(mac, ip, ip_dns, ip_gateway, subnet);    
-  api_status = Agentuino.begin();                         // Begin SNMP agent
+  Ethernet.begin(mac, ip, ip_dns, ip_gateway, subnet);
+  api_status = Agentuino.begin();                        
  
   if ( api_status == SNMP_API_STAT_SUCCESS ) {
     Agentuino.onPduReceive(pduReceived);
@@ -189,11 +210,13 @@ void setup() {
 }
  
 void loop(){ 
-  Agentuino.listen();                                      // Listen/Handle for incoming SNMP requests
-  
+  // Handle for incoming SNMP requests
+  Agentuino.listen();                                      
+
   if(millis()-prevMillis>2000) {  
     humidity = dht.readHumidity();
     temperature = dht.readTemperature();
+    current = deltaCurrent();
     prevMillis = millis();
   }  
 }
